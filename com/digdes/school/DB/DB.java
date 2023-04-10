04380830005286dd1121b13d343298b40720921f
Roman Gontar <ashenoooone@gmail.com>
2023-04-10 16:21:44 +0400

    feat: отбор по одному условию

--- com/digdes/school/DB/DB.java
+++ com/digdes/school/DB/DB.java
@@ -1,8 +1,5 @@
 package com.digdes.school.DB;
-
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class DB {
     private final List<Map<String, Object>> table = new ArrayList<>();
@@ -94,7 +91,7 @@ public class DB {
                 condition = splittedValue[1].trim();
             }
         }
-        System.out.println(condition);
+        System.out.println("------------------------------------------------");
         return filterData(condition);
     }
 
@@ -103,65 +100,116 @@ public class DB {
         return this.getTable();
     }
 
-    public List<Map<String, Object>> filterData(String condition) {
-//        like ilike < <= > >= == != and or
+    public List<Map<String, Object>> filterData(String filterString) {
         List<Map<String, Object>> filteredRows = new ArrayList<>();
-        List<Map<String, Object>> rows = this.getTable();
-        System.out.println(condition);
-        Pattern pattern = Pattern.compile("'(.*)' *([^\\d ]*) * '*([^']+)'*$| ");
-        Matcher matcher = pattern.matcher(condition.trim());
-        if (matcher.find()) {
-            Column column = findColumnByName(matcher.group(1));
-            String operator = matcher.group(2);
-            if (!column.getAvailableOperators().contains(operator))
-                throw new IllegalArgumentException("Неподдерживаемый оператор");
-            Class<?> valueType = column.getType().getTypeClass();
-            Object parsedValue = matcher.group(3);
-            if (valueType == String.class) {
-                parsedValue = (String) parsedValue;
-            } else if (valueType == Boolean.class) {
-                parsedValue = (Boolean) parsedValue;
-            } else if (valueType == Long.class) {
-                parsedValue = (Long) parsedValue;
-            } else if (valueType == Double.class) {
-                parsedValue = (Double) parsedValue;
-            } else {
-                throw new IncompatibleColumnTypeException("Неподдерживаемый тип столбца: " + valueType.getSimpleName());
-            }
-            for (Map<String, Object> row : this.getTable()) {
-                for (Map.Entry<String, Object> entry : row.entrySet()) {
-                    if (Objects.equals(entry.getKey(), column.getName())) {
-                        switch (operator) {
-                            case ">":
-                                
-                                break;
-                            case ">=":
-                                break;
-                            case "==":
-                                break;
-                            case "!=":
-                                break;
-                            case "like":
-                                break;
-                            case "ilike":
-                                break;
-                        }
-                    }
+        String[] filters = filterString.split("(?i)\\s+(and|or)\\s+");
+        for (Map<String, Object> row : this.getTable()) {
+            boolean matches = false;
+            for (String filter : filters) {
+                String[] parts = filter.split("\\s+");
+                if (parts.length != 3) {
+                    throw new IllegalArgumentException("Invalid filter string: " + filterString);
+                }
+                String columnName = parts[0].replaceAll("^'|'$", "");
+                Column column = findColumnByName(columnName);
+                String operator = parts[1];
+                String valueString = parts[2].replaceAll("^'|'$", "");
+                Object value = row.get(columnName);
+                Object value2;
+                if (!column.getAvailableOperators().contains(operator)) {
+                    throw new IllegalArgumentException("Неподдерживаемый оператор");
+                }
+                if (value instanceof Long) {
+                    value2 = Long.valueOf(valueString);
+                } else if (value instanceof String) {
+                    value2 = valueString;
+                } else if (value instanceof Double) {
+                    value2 = Double.valueOf(valueString);
+                } else if (value instanceof Boolean) {
+                    value2 = Boolean.valueOf(valueString);
+                } else {
+                    throw new IllegalArgumentException("Неподдерживаемый тип переменной");
+                }
+                switch (operator) {
+                    case "<":
+                        if (lessThan(value, value2)) matches = true;
+                        break;
+                    case "<=":
+                        if (lessThanOrEqual(value, value2)) matches = true;
+                        break;
+                    case ">":
+                        System.out.println(greaterThan(value, value2));
+                        if (greaterThan(value, value2)) matches = true;
+                        break;
+                    case ">=":
+                        if (greaterThanOrEqual(value, value2)) matches = true;
+                        break;
+                    case "==":
+                        if (equal(value, value2)) matches = true;
+                        break;
+                    case "!=":
+                        if (notEqual(value, value2)) matches = true;
+                        break;
+                    case "like":
+                        if (like(value, value2)) matches = true;
+                        break;
+                    case "ilike":
+                        if (iLike(value, value2)) matches = true;
+                        break;
+                }
+
+                if (!matches) {
+                    break;
                 }
             }
+            if (matches) {
+                filteredRows.add(row);
+            }
         }
+        return filteredRows;
+    }
 
 
-        return this.getTable();
+    private static boolean like(Object value1, Object value2) {
+        String pattern = ((String) value2).replace("%", ".*");
+        return ((String) value1).matches(pattern);
+    }
+
+    private static boolean iLike(Object value1, Object value2) {
+        String pattern = ((String) value2).replace("%", ".*").toLowerCase();
+        return ((String) value1).toLowerCase().matches(pattern);
+    }
+
+    private static boolean lessThan(Object value1, Object value2) {
+        return ((Comparable) value1).compareTo(value2) < 0;
+    }
+
+    private static boolean lessThanOrEqual(Object value1, Object value2) {
+        return ((Comparable) value1).compareTo(value2) <= 0;
+    }
+
+    private static boolean greaterThan(Object value1, Object value2) {
+        return ((Comparable) value1).compareTo(value2) > 0;
+    }
+
+    private static boolean greaterThanOrEqual(Object value1, Object value2) {
+        return ((Comparable) value1).compareTo(value2) >= 0;
+    }
+
+    private static boolean equal(Object value1, Object value2) {
+        if (value1 == null || value2 == null) {
+            return value1 == null && value2 == null;
+        } else {
+            return value1.equals(value2);
+        }
+    }
+
+    private static boolean notEqual(Object value1, Object value2) {
+        return !equal(value1, value2);
     }
 
     public List<Map<String, Object>> getTable() {
-        List<Map<String, Object>> copyList = new ArrayList<>();
-        for (Map<String, Object> originalMap : this.table) {
-            Map<String, Object> copyMap = new HashMap<>(originalMap);
-            copyList.add(copyMap);
-        }
-        return copyList;
+        return this.table;
     }
 
 
--- com/digdes/school/Main.java
+++ com/digdes/school/Main.java
@@ -7,6 +7,6 @@ public class Main {
         JavaSchoolStarter starter = new JavaSchoolStarter();
         System.out.println(starter.execute("INSERT VALUES 'lastname' = 'Федоров', 'id'=3, 'age'=40, 'active'=true"));
         System.out.println(starter.execute("INSERT VALUES 'lastname' = 'Петров', 'id'=3, 'age'=40, 'active'=true"));
-        System.out.println(starter.execute("UPDATE VALUES ‘active’=false, ‘cost’=10.1 WHERE 'age' >= 30"));
+        System.out.println(starter.execute("UPDATE VALUES ‘active’=false, ‘cost’=10.1 WHERE 'age' > 39"));
     }
 }
